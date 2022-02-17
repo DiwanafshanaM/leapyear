33e8fde7ec401ba186607cda14ce2725cf1f32c2
Acer <Acer@DESKTOP-F9ONNJE>
2022-02-17 16:03:14 +0530

    leapyear

--- Leapyear/src/leapyear/Leapyear.java
+++ Leapyear/src/leapyear/Leapyear.java
@@ -6,21 +6,20 @@ public class Leapyear {
 		int year = 1957;
 	    boolean leap = false;
 
-	    // if the year is divided by 4
+	   
 	    if (year % 4 == 0) {
 
-	      // if the year is century
+	     
 	      if (year % 100 == 0) {
 
-	        // if year is divided by 400
-	        // then it is a leap year
+	        
 	        if (year % 400 == 0)
 	          leap = true;
 	        else
 	          leap = false;
 	      }
 	      
-	      // if the year is not century
+	      
 	      else
 	        leap = true;
 	    }
